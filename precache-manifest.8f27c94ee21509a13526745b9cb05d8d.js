9dc7d46c2d758473fad35d3970c3578dfa043a05
Alex Bakhteyev <a.bakhteyev@gmail.com>
2020-01-30 15:11:16 -0500

    Updates

---
+++ precache-manifest.8f27c94ee21509a13526745b9cb05d8d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da58e907ff684d9eb21c412d22489fba",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "4088f5bc70a9cf7807a7",
+    "url": "/goit-react-hw-02-feedback/static/js/2.e2fcffeb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/goit-react-hw-02-feedback/static/js/2.e2fcffeb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "58855bab74418f46f0bd",
+    "url": "/goit-react-hw-02-feedback/static/js/main.fd1be819.chunk.js"
+  },
+  {
+    "revision": "123d81bea882563839a7",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.d87da01c.js"
+  }
+]);
