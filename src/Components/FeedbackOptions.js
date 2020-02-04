6b719342dc612f9859ede25736c80567dc400a10
Alex Bakhteyev <a.bakhteyev@gmail.com>
2020-02-04 14:44:22 -0500

    upates to PropTypes

--- src/Components/FeedbackOptions.js
+++ src/Components/FeedbackOptions.js
@@ -19,7 +19,7 @@ const FeedbackOptions = ({ onLeaveFeedback, options }) => (
 
 FeedbackOptions.propTypes = {
   onLeaveFeedback: PropTypes.func.isRequired,
-  options: PropTypes.array.isRequired,
-};
+  options: PropTypes.arrayOf(PropTypes.string.isRequired)
+   };
 
 export default FeedbackOptions;
